857d00280fe0b548547c538d52f10a5ab68b05a4
Andyiscool <andy.xiao@live.com>
2017-07-09 10:49:20 -0700

    Add files via upload

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,19 @@
+/**
+ * Created by andyxiao on 7/8/17.
+ */
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Andy!");
+
+        int myfirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myfirstNumber * 2;
+
+        int myTotal = myfirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
+    }
+}
